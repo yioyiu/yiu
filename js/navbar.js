fb70638db07c1501b06ae9510a64a8d9eb8056a6
yioyiu <zyuy2001@hotmail.com>
2022-05-12 09:51:15 +0800

    1.0.2

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,16 @@
-var navbar = document.getElementById("navbar");
-        var menu = document.getElementById("menu");
-        window.onscroll = function () {
-            if (window.pageYOffset >= menu.offsetTop) {
-                navber.classlist.add("sticky");
-            }
-            else {
-                navbar.classList.remove("sticky");
-            }
-        }
+
+
+
+window.addEventListener('scroll', () => {
+    // 提取nav元素让nav的元素叫nav
+    let nav = document.querySelector('nav')
+    // 给nav加个class“sticky”（条件：滚轮>100）
+    nav.classList.toggle('sticky', window.scrollY > 100)
+})
+window.addEventListener('scroll', () => {
+
+    // 提取img元素让img的元素叫logo
+    let logo = document.querySelector('img')
+    // 给logo加个class“sticky”（条件：滚轮>100）
+    logo.classList.toggle('sticky', window.scrollY > 100)
+})
